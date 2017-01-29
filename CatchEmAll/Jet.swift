ad1e2abea889438b4858c18c30fa072b13732a6d
ac <ac@Jaxxs-MBP.home>
2017-01-29 18:21:46 -0500

    completed

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+class Jet: Plane {
+    
+    override func climb() {
+        let newAltitude = altitude + maxAltitude/5
+        if inFlight {
+            if newAltitude < maxAltitude {
+                altitude = newAltitude
+            }
+            else {
+                altitude = maxAltitude
+            }
+            decelerate()
+        } //if inFlight
+        
+    }
+    
+    override func dive() {
+        let newAltitude = altitude - maxAltitude/5
+        if inFlight {
+            if newAltitude > 0 {
+                altitude = newAltitude
+            }
+            else {
+                altitude = 0
+            }
+            accelerate()
+        } //if inFlight
+        
+    }
+    
+    func afterburner() {
+        if inFlight && speed == maxSpeed {
+            speed = 2 * maxSpeed
+        }
+    }
+    
+}
--- CatchEmAll/RaceCar.swift
+++ CatchEmAll/RaceCar.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+
+class RaceCar: Car {
+    let driver:String
+    var sponsors:[String]
+    
+    init(name:String, weight:Double, maxSpeed:Double , transmission:Transmission, cylinders:Int, milesPerGallon:Double ,driver:String, sponsors:[String]) {
+        self.driver = driver
+        self.sponsors = sponsors
+        super.init(name: name, weight: weight,maxSpeed: maxSpeed, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon )
+        
+    }
+    
+    override func accelerate() {
+        let newSpeed = speed + maxSpeed/5
+        if newSpeed <= maxSpeed {
+            speed = newSpeed
+        }
+        else {
+            speed = maxSpeed
+        }
+        
+    }
+    
+    override func decelerate() {
+        let newSpeed = speed - maxSpeed/5
+        if newSpeed > 0 {
+            speed = newSpeed
+        }
+        else {
+            speed = 0
+        }
+        
+    }
+    
+    func driftRight() {
+        if speed > 0 {
+            let newHeading = heading + 90
+            if newHeading <= 360 {
+                heading = newHeading
+            }
+            else {
+                heading = newHeading - 360
+            }
+            speed -= speed/4
+        } //if speed > 0
+        
+    }
+    
+    func driftLeft() {
+        if speed > 0 {
+            let newHeading = heading - 90
+            if newHeading >= 0 {
+                heading = newHeading
+            }
+            else {
+                heading = 360 + newHeading  //-ve value
+            }
+            speed -= speed/4
+        } //if speed > 0
+        
+    }
+    
+    
+}
